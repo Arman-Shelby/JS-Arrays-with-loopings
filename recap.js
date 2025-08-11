1686047a34d895763480d745a0640a10c5ef3d7f
Arman Mir <mirarman8583@gmail.com>
2025-08-11 16:00:41 +0600

    recap

---
+++ recap.js
@@ -0,0 +1,23 @@
+const books  = ['Physics' , 'Java Script ', 'CSE', 'Math'];
+
+for(let i = 0; i<books.length; i++ ){
+    console.log(books[i]);
+}
+
+
+
+const rev_books = books.reverse()
+
+const sorted_books = books.sort();
+
+const gadgets = [
+    {name: 'Laptop', price: 35000, color: 'Black' , brand: 'lenovo'},
+    {name: 'smartwatch', price: 12000, color= 'green', brand: 'samsung'},
+    {name: 'phone' , price= 35000 , color: 'skyblue', brand: 'samsung'}
+]
+
+const matrix = [
+    [2 , 8 , 12],
+    [5 ,7 ,10],
+    [12 , 9 ,18]
+]
